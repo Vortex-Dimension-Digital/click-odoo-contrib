be00811fd53226cfbabe8509aa2f5cd920785373
arch-fan <juanserracines@gmail.com>
2025-11-24 07:10:26 +0100

    [FORMAT]

--- click_odoo_contrib/initdb.py
+++ click_odoo_contrib/initdb.py
@@ -108,10 +108,12 @@ def odoo_createdb(
                     [("code", "ilike", country)], limit=1
                 )
                 if chosen_country:
-                    env["res.company"].browse(1).write({
-                        "country_id": chosen_country.id,
-                        "currency_id": chosen_country.currency_id.id,
-                    })
+                    env["res.company"].browse(1).write(
+                        {
+                            "country_id": chosen_country.id,
+                            "currency_id": chosen_country.currency_id.id,
+                        }
+                    )
                     timezones = country_timezones.get(country, [])
                     if len(timezones) == 1:
                         env["res.users"].search([]).write({"tz": timezones[0]})
